8250897f46356e09c75aa8684700c3aac1dd6c9c
Elmo Trolla <elmo.trolla@fdkz.net>
2016-11-17 16:33:35 +0200

    update windows build scripts: turn on optimizations, shrink exe size 3x, add upx support

--- build64-vc12.bat
+++ build64-vc12.bat
@@ -1,4 +1,4 @@
-:: this builds the debug version?
+:: this builds the release version
 
 @echo off
 
@@ -11,48 +11,51 @@
 :: /Zi : Select the type of debugging information created for your program and whether this information is kept in object (.obj) files or in a program database (PDB).
 ::       Produces a program database (PDB) that contains type information and symbolic debugging information for use with the debugger.
 :: /MD : Causes the application to use the multithread-specific and DLL-specific version of the run-time library. Defines _MT and _DLL and causes the compiler to place the library name MSVCRT.lib into the .obj file.
+:: /Gy : Enables function-level linking. Prevents unused functions from being included.
+:: /Ox : Uses maximum optimization (/Ob2gity /Gs).Uses maximum optimization (/Ob2gity /Gs).
+:: /OPT:REF : eliminates functions and data that are never referenced (depends on /Gy, but seems to do nothing for this program)
 ::
 
 :: TODO: how should i have known to use /MD? SDL2 has written nowhere how it was compiled.
-:: well whadoyouknow! ending even a fucking COMMENT with a caret does something weird
-
-::cl /FC /Zi /MD
-::cl /FC /Zi /Ox /MD
 
 :: https://www.libsdl.org/release/SDL2-devel-2.0.4-VC.zip
 :: https://sourceforge.net/projects/glew/files/glew/1.13.0/glew-1.13.0-win32.zip/download
 
+:: well whadoyouknow! ending even a fucking COMMENT with a caret does something weird
 
 set SDL2DIR=.\lib\SDL2-devel-2.0.4-VC
 set GL3WDIR=.\lib\gl3w
 set IMGUIDIR=.\lib\imgui
 set BUILDDIR=.\build
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
-rmdir build
+rmdir /S /Q build
 mkdir build
 
-rem    /I %IMGUIDIR%\examples\sdl_opengl3_example ^
-rem    .\imgui_impl_sdl_gl3.cpp ^
 
-cl /FC /Zi /MD /Fo%BUILDDIR%\ /Fd%BUILDDIR%\ /Fm%BUILDDIR%\ /Fe%BUILDDIR%\aniplot.exe ^
+cl /FC /MD /Ox /Gy /Fo%BUILDDIR%\ /Fd%BUILDDIR%\ /Fm%BUILDDIR%\ /Fe%BUILDDIR%\aniplot.exe ^
     main.cpp ^
-    .\lib\imgui\examples\sdl_opengl3_example\imgui_impl_sdl_gl3.cpp ^
-    .\lib\imgui\imgui.cpp .\lib\imgui\imgui_demo.cpp .\lib\imgui\imgui_draw.cpp ^
-    .\lib\gl3w\GL\gl3w.c ^
+    %IMGUIDIR%\examples\sdl_opengl3_example\imgui_impl_sdl_gl3.cpp ^
+    %IMGUIDIR%\imgui.cpp %IMGUIDIR%\imgui_draw.cpp ^
+    %IMGUIDIR%\imgui_demo.cpp ^
+    %GL3WDIR%\GL\gl3w.c ^
+    /D_HAS_EXCEPTIONS=0 /DNDEBUG ^
     /I %IMGUIDIR% ^
     /I %IMGUIDIR%\examples\sdl_opengl3_example ^
     /I %SDL2DIR%\include ^
     /I %GL3WDIR% ^
-    /D_HAS_EXCEPTIONS=0 ^
     SDL2.lib SDL2main.lib user32.lib opengl32.lib ws2_32.lib ^
     /link /SUBSYSTEM:CONSOLE ^
-          /LIBPATH:"%SDL2DIR%\lib\x64"
+          /LIBPATH:"%SDL2DIR%\lib\x64" /OPT:REF /OPT:ICF
 
-rem    /O2 /GL ^
 
-rmdir dist
+rmdir /S /Q dist
 mkdir dist
 copy %BUILDDIR%\aniplot.exe .\dist\
 copy %SDL2DIR%\lib\x64\SDL2.dll .\dist\
+
+if exist "upx.exe" (
+    upx --lzma .\dist\aniplot.exe
+    upx --lzma .\dist\SDL2.dll
+)
